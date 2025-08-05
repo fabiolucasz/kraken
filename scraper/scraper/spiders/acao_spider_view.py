a919be60383c8f9ad5e22bb0f0ec3a581b34a543
fabiolucasz <fabio_9.9@hotmail.com>
2025-08-05 10:05:12 -0300

    update acoes database

--- scraper/scraper/spiders/acao_spider_view.py
+++ scraper/scraper/spiders/acao_spider_view.py
@@ -35,22 +35,22 @@ class AcaoSpider(scrapy.Spider):
 
             # KPIs
             kpis = [k.strip().replace(" ", "_") for k in response.css('div._card-header div span::text').getall() if k.strip()]
-            kpi_values = [v.strip().replace(" ", "").replace("%", "").replace("R$", "").replace(",", ".") for v in response.css('div._card-body span::text').getall() if v.strip()]
+            kpi_values = [v.strip().replace(" ", "").replace("%", "").replace("R$", "") for v in response.css('div._card-body span::text').getall() if v.strip()]
             while len(kpi_values) < len(kpis):
                 kpi_values.append("")
             self.dados_kpi.append(dict(zip(kpis, kpi_values), Papel=papel))
 
             # Indicadores
             indicadores = [i.strip().replace(" ", "_").replace(f"DIVIDEND_YIELD__-_{response.meta['papel']}", "DY") for i in response.css('div.cell span.d-flex::text').getall() if i.strip()]
-            indicadores_values = [iv.strip().replace(",", ".").replace("%", "") for iv in response.css('div.value span::text').getall() if iv.strip()]
+            indicadores_values = [iv.strip().replace("%", "") for iv in response.css('div.value span::text').getall() if iv.strip()]
             while len(indicadores_values) < len(indicadores):
                 indicadores_values.append("")
             self.dados_indicadores.append(dict(zip(indicadores, indicadores_values), Papel=papel))
 
             # Informações da ação
             info = [i.strip().replace(" ", "_").replace("Segmento_de_Listagem", "Liquidez_Média_Diária").replace("Free_Float", "Segmento_de_Listagem").replace("Tag_Along", "Free_Float").replace("Liquidez_Média_Diária", "Tag_Along") for i in response.css('div.cell span.title::text').getall() if i.strip()]
-            info_values_div = [iv.strip().replace(" ", "").replace("%", "").replace("R$", "").replace(".", "").replace(",", ".") for iv in response.css('span.value div.detail-value::text').getall() if iv.strip()]
-            info_values_text = [iv.strip().replace(" ", "_").replace("%", "").replace("R$", "").replace(",", ".") for iv in response.css('div.cell span.value::text').getall() if iv.strip()]
+            info_values_div = [iv.strip().replace(" ", "").replace("%", "").replace("R$", "") for iv in response.css('span.value div.detail-value::text').getall() if iv.strip()]
+            info_values_text = [iv.strip().replace(" ", "_").replace("%", "").replace("R$", "") for iv in response.css('div.cell span.value::text').getall() if iv.strip()]
             info_values = info_values_div + info_values_text
             while len(info_values) < len(info):
                 info_values.append("")
@@ -71,25 +71,21 @@ def run_scraper():
     info_df = pd.DataFrame(AcaoSpider.dados_info).fillna("")
     img_df = pd.DataFrame(AcaoSpider.dados_img).fillna("")
 
-    kpi_df.to_csv("acoes_consolidadas_kpi.csv", index=False, encoding='utf-8')
-    indicadores_df.to_csv("acoes_consolidadas_indicadores.csv", index=False, encoding='utf-8')
-    info_df.to_csv("acoes_consolidadas_info.csv", index=False, encoding='utf-8')
-    img_df.to_csv("acoes_consolidadas_img.csv", index=False, encoding='utf-8')
-    merge_csv()
+    kpi_df.to_csv("acoes_consolidadas_kpi_view.csv", index=False, encoding='utf-8')
+    indicadores_df.to_csv("acoes_consolidadas_indicadores_view.csv", index=False, encoding='utf-8')
+    info_df.to_csv("acoes_consolidadas_info_view.csv", index=False, encoding='utf-8')
+    img_df.to_csv("acoes_consolidadas_img_view.csv", index=False, encoding='utf-8')
 
     print("Todos os dados foram salvos com sucesso!")
 
 def merge_csv():
-    df1 = pd.read_csv("acoes_consolidadas_kpi.csv")
-    df2 = pd.read_csv("acoes_consolidadas_indicadores.csv")
-    df3 = pd.read_csv("acoes_consolidadas_info.csv")
-    df4 = pd.read_csv("acoes_consolidadas_img.csv")
+    df1 = pd.read_csv("acoes_consolidadas_kpi_view.csv")
+    df2 = pd.read_csv("acoes_consolidadas_indicadores_view.csv")
+    df3 = pd.read_csv("acoes_consolidadas_info_view.csv")
+    df4 = pd.read_csv("acoes_consolidadas_img_view.csv")
     df = pd.merge(df1, df2).merge(df3).merge(df4)
-    df['P/L'] = df['P/L'].apply(remover_segundo_ponto)
-    df['PAYOUT'] = df['PAYOUT'].apply(remover_segundo_ponto)
-    df['ROE'] = df['ROE'].apply(remover_segundo_ponto)
     df = df.drop(columns=["carteira_investidor_10"])
-    df.to_csv("acoes.csv", index=False, encoding='utf-8')
+    df.to_csv("acoes_view.csv", index=False, encoding='utf-8')
 
 def remover_segundo_ponto(val):
     if pd.isna(val):
@@ -104,4 +100,5 @@ def remover_segundo_ponto(val):
 
 if __name__ == "__main__":
     run_scraper()
+    merge_csv()
 
